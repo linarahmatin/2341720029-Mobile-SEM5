46b8ac44aeadb4903575bd10762f18f8faafa732
linarahmatin <linarahmatin9@gmail.com>
2025-10-26 20:05:07 +0700

    update P9

---
+++ Pertemuan 9/README.md
@@ -0,0 +1,14 @@
+# Pemrograman Mobile - Pertemuan 9
+
+NAMA: Sesy Tana Lina Rahmatin
+KELAS: TI-3F
+NIM: 2341720029
+
+# Praktikum 1
+
+output:
+
+# Praktikum 2
+
+output:
+
---
+++ Pertemuan 9/kamera_flutter/lib/widget/displaypicture_screen.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+// Widget untuk menampilkan gambar yang diambil.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Display the Picture - sesy - 2341720029'),
+      ),
+      // Gambar disimpan sebagai file di perangkat.
+      // Gunakan constructor `Image.file` dengan path yang diberikan.
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
