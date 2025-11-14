8481ad7237c6d71f8bfc5c1ac979a8567f10d7d5
linarahmatin <linarahmatin9@gmail.com>
2025-11-14 16:54:06 +0700

    Praktikum 1

---
+++ Pertemuan 12/stream_lina/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'stream.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stream - Lina',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const StreamHomePage(),
+    );
+  }
+}
+
+class StreamHomePage extends StatefulWidget {
+  const StreamHomePage({super.key});
+
+  @override
+  State<StreamHomePage> createState() => _StreamHomePageState();
+}
+
+class _StreamHomePageState extends State<StreamHomePage> {
+  // Properti
+  Color bgColor = Colors.blueGrey;
+  late ColorStream colorStream;
+
+  // Ubah isi method changeColor()
+  void changeColor() {
+    colorStream.getColors().listen((eventColor) {
+      setState(() {
+        bgColor = eventColor;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    colorStream = ColorStream();
+    changeColor();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stream - Lina'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: bgColor,
+        ),
+      ),
+    );
+  }
+}
